a8575c337dc2d796821bf36d900fb32bf5a1123d
RaviPatel021 <ravi_patel@utexas.edu>
2024-11-01 21:53:10 -0500

    added debug mode

--- app.py
+++ app.py
@@ -152,5 +152,5 @@ def determine_winner(user, computer):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Default to 5000 if not specified by Render
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
